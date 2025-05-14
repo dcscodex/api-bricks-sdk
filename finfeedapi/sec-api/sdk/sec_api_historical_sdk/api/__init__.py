6e71f8027e601609c94a4b1b3c82bfc2f88b0ee9
Artur Pietrzyk <Svisstack@users.noreply.github.com>
2025-05-14 10:42:18 +0200

    ref

--- finfeedapi/sec-api/sdk/sec_api_historical_sdk/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from sec_api_historical_sdk.api.content_extraction_api import ContentExtractionApi
-from sec_api_historical_sdk.api.filing_metadata_api import FilingMetadataApi
-from sec_api_historical_sdk.api.full_text_search_api import FullTextSearchApi
-

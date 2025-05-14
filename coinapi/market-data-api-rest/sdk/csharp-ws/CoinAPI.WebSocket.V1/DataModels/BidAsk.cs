9adeeb36d9cd1321bc0a234f60193f10dbcca25c
Artur Pietrzyk <Svisstack@users.noreply.github.com>
2025-05-14 14:07:58 +0200

    flow fix

--- coinapi/market-data-api-rest/sdk/csharp-ws/CoinAPI.WebSocket.V1/DataModels/BidAsk.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CoinAPI.WebSocket.V1.DataModels
-{
-    public struct BidAsk
-    {
-        public decimal price { get; set; }
-        public decimal size { get; set; }
-    }
-}
--- coinapi/market-data-api-rest/sdk/java-websocket/deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn clean javadoc:jar deploy -Dmaven.test.skip=true -P release-sign-artifacts
